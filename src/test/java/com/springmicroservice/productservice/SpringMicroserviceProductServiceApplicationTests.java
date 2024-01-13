e1336464a6f6f89f5d660621df3e84468dc64f78
kanandvardhan <kanandvardhan@gmail.com>
2024-01-13 23:58:17 +0530

    Added Product Repository, service, and controller

---
+++ src/test/java/com/springmicroservice/productservice/SpringMicroserviceProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmicroservice.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
