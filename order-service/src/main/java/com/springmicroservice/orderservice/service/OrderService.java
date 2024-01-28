18a236c300987a332434136637fbb71a9458e21c
kanandvardhan <kanandvardhan@gmail.com>
2024-01-28 18:00:46 +0530

    created order-service

---
+++ order-service/src/main/java/com/springmicroservice/orderservice/service/OrderService.java
@@ -0,0 +1,39 @@
+package com.springmicroservice.orderservice.service;
+
+import com.springmicroservice.orderservice.dto.OrderLineItemsDto;
+import com.springmicroservice.orderservice.dto.OrderRequest;
+import com.springmicroservice.orderservice.model.Order;
+import com.springmicroservice.orderservice.model.OrderLineItems;
+import com.springmicroservice.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream().map(this::maptoDto).toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems maptoDto(OrderLineItemsDto orderLineItemsDto){
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItems.getQuantity());
+        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
+
+        return orderLineItems;
+    }
+}
