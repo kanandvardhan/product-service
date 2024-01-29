478170f6de3b98769d557220ab1b53332b12ac24
kanandvardhan <kanandvardhan@gmail.com>
2024-01-29 09:54:03 +0530

    feature of placing order after checking inventory

--- order-service/src/main/java/com/springmicroservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/springmicroservice/orderservice/controller/OrderController.java
@@ -3,19 +3,22 @@ package com.springmicroservice.orderservice.controller;
 import com.springmicroservice.orderservice.dto.OrderRequest;
 import com.springmicroservice.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
     }
--- order-service/src/main/java/com/springmicroservice/orderservice/model/Order.java
+++ order-service/src/main/java/com/springmicroservice/orderservice/model/Order.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 import java.util.List;
 
 @Entity
-@Table(name="t_orders")
+@Table(name = "t_orders")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,7 +17,7 @@ import java.util.List;
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String orderNumber;
     @OneToMany(cascade = CascadeType.ALL)
