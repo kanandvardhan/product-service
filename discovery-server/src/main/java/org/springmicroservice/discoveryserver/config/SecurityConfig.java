fc10c2b3d34aad91220eb12068bebfea82e19135
kanandvardhan <kanandvardhan@gmail.com>
2024-02-04 14:25:24 +0530

    dockerised application

--- discovery-server/src/main/java/org/springmicroservice/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/org/springmicroservice/discoveryserver/config/SecurityConfig.java
@@ -11,7 +11,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
-
         return httpSecurity.build();
     }
 }
